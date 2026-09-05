169092fac53d93e671b1a018113d9d9f4152ff53
dongarsing07 <dongarsingpatil07@gmail.com>
2026-09-05 17:24:52 +0530

    Prepare frontend for production

--- frontend/frontend/src/socket.js
+++ frontend/frontend/src/socket.js
@@ -1,8 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  transports: ["websocket"],
-  withCredentials: true,
-});
+const socket = io(
+  process.env.REACT_APP_API_URL?.replace("/api", ""),
+  {
+    transports: ["websocket"],
+    withCredentials: true,
+  }
+);
 
 export default socket;
